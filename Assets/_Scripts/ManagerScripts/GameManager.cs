02ed94916369bc3603991086a73931c24efc1b26
rikhilunity3d <rikhil.unity3d@gmail.com>
2022-09-23 18:33:42 +0530

    DressUp Christmas Added

--- Assets/_Scripts/ManagerScripts/GameManager.cs
+++ Assets/_Scripts/ManagerScripts/GameManager.cs
@@ -180,21 +180,22 @@ public class GameManager : GenericSingleton<GameManager>
         // Setting camera orthographic size accroding to the background image of
         //Game.If you want to calculate width and height use the below commented
         //code and comment below line
-        Camera.main.orthographicSize = backgroundImage.bounds.size.y / 2;
+        
 
         // this code will calculate camera orthograhpic size using width and height ratio
         // uncomment this block of code
 
-        /*float screenRatio = (float)Screen.width / (float) Screen.height;
+        float screenRatio = (float)Screen.width / (float) Screen.height;
         float targetRatio = backgroundImage.bounds.size.x / backgroundImage.bounds.size.y;
-        if(screenRatio >= targetRatio)
+        float differenceInSize = targetRatio / screenRatio;
+        Camera.main.orthographicSize = backgroundImage.bounds.size.y / 2 * differenceInSize;
+        /*if (screenRatio >= targetRatio)
         {
             Camera.main.orthographicSize = backgroundImage.bounds.size.y / 2;
         }
         else
         {
-            float differenceInSize = targetRatio / screenRatio;
-            Camera.main.orthographicSize = backgroundImage.bounds.size.y / 2*differenceInSize;
+            
         }*/
     }
 
